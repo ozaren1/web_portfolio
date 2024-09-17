0786fa088d97724277db58e73a5c14598879838d
Pavel <ozareniep@gmail.com>
2024-09-17 03:03:43 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
   return (
     <Container>
       <Header/>
-      <Portfolio/>
     </Container>
    
   );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import styles from './Header.module.scss';
-
+import {Routes, Route, Link} from 'react-router-dom'
+import Portfolio from '../Portfolio';
+//import Contact from '../Contact';
 
 function Header() {
   return (
-    <header className={styles.header}>
-     
-    <ul>
-        <li><a href='/portfolio'>portfolio </a></li>
-        <li className={styles.logo}><a href='/'>Merkulova Daria</a></li>
-        <li><a href='/contact'>contact </a></li>
-    </ul>
-    
-  </header>
-   
+    <>
+      <header className={styles.header}>
+      <ul>
+          <li><Link to='/portfolio'>portfolio </Link></li>
+          <li className={styles.logo}><Link to='/'>Merkulova Daria</Link></li>
+          <li><Link to='/contact'>contact </Link></li>
+      </ul>
+      </header>
+      <Routes>
+        <Route path='/portfolio' element={<Portfolio />}/>
+      </Routes>
+    </>
   );
 }
 export default Header;
