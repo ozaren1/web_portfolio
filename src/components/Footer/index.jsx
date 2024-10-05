10cd61cb3c0ca773b0af0bb616332cdf816f895e
Pavel <ozareniep@gmail.com>
2024-10-06 00:59:52 +0300

    before I installed Express

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,11 +8,11 @@ export default function Footer() {
                     <a href=""><img src="/assets/image/icon/whatsapp.svg" alt="Whatsapp-logo" /></a>
                     <a href=""><img src="/assets/image/icon/VK_Logo.svg" alt="VK-logo" /></a>
                     <a href=""><img src="/assets/image/icon/Telegram-logo.svg" alt="Telegram-logo" /></a>
-                    <a href=""><img src="/assets/image/icon/instagram.svg" alt="Instagram-logo" /></a>
+                    <a href=""><img src="/assets/image/icon/link.svg" alt="Telegram-logo" /></a>
                 </div>
                 <div></div>
-                <div className='site-by'>Site by </div>
-            
+                <div className={styles.site_by}><span>Site by </span><a href=""><img src="/assets/image/icon/oko.svg" alt="" /></a></div>
+                
             </Container>
         </footer>
     )
